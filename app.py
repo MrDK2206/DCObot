cb3ae330fd917f22baf4c00f5e801abe75cf3adc
Dhruvil Prajapati <dhruvilprajapati1154b@gmail.com>
2025-09-09 13:01:40 +0530

    Initial commit of Medical chatbot

--- app.py
+++ app.py
@@ -15,8 +15,12 @@ app = Flask(__name__)
 
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+#PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+#OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+# Load keys from environment
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
